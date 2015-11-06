39c1e2790fc9b6285adc3f01e2b491f345aaa842
oeuillot <olivier.oeuillot@gmail.com>
2015-11-06 17:19:03 +0100

    Fix code

--- xpl-scripts.js
+++ xpl-scripts.js
@@ -10,81 +10,84 @@ var ScriptsEngine = require("./lib/scriptsEngine");
 
 commander.version(require("./package.json").version);
 commander.option("--deviceAliases <path>", "Device aliases (path or string)");
+commander.option("--scriptsLogPath <path>", "Logs directory");
+commander.option("--logRotate", "Enable scripts logs rotate");
+commander.option("--logRotateSize <size>", "Max log size");
+commander.option("--logRotateKeep <keep>", "Logs count", parseInt);
+commander.option("--logRotateCompress", "Compress logs rotate");
 
 Xpl.fillCommander(commander);
 
-commander.command('start').action(
-    function(params) {
-      console.log("Start", params);
+commander.command('start').action(function(params) {
+  console.log("Start", params);
 
-      var deviceAliases = commander.deviceAliases;
-      if (deviceAliases) {
-        if (deviceAliases.indexOf('=') >= 0) {
-          var ds = {};
-          commander.deviceAliases = ds;
+  var deviceAliases = commander.deviceAliases;
+  if (deviceAliases) {
+    if (deviceAliases.indexOf('=') >= 0) {
+      var ds = {};
+      commander.deviceAliases = ds;
 
-          var js = deviceAliases.split(',');
-          for (var i = 0; i < js.length; i++) {
-            var j = js[i].split('=');
-            if (j.length === 2) {
-              ds[j[0].trim()] = j[1].trim();
-            }
-          }
-
-        } else {
-          commander.deviceAliases = {};
-          deviceAliases.split(",").forEach(function(path) {
-            var r = require(path);
-            for ( var n in r) {
-              if (!r.hasOwnProperty(n)) {
-                continue;
-              }
-              commander.deviceAliases[n] = r[n];
-            }
-          });
+      var js = deviceAliases.split(',');
+      for (var i = 0; i < js.length; i++) {
+        var j = js[i].split('=');
+        if (j.length === 2) {
+          ds[j[0].trim()] = j[1].trim();
         }
-
-        debug("DeviceAliases=", commander.deviceAliases);
       }
 
-      if (!commander.xplSource) {
-        var hostName = os.hostname();
-        if (hostName.indexOf('.') > 0) {
-          hostName = hostName.substring(0, hostName.indexOf('.'));
+    } else {
+      commander.deviceAliases = {};
+      deviceAliases.split(",").forEach(function(path) {
+        var r = require(path);
+        for ( var n in r) {
+          if (!r.hasOwnProperty(n)) {
+            continue;
+          }
+          commander.deviceAliases[n] = r[n];
         }
+      });
+    }
 
-        commander.xplSource = "tsx-script." + hostName;
+    debug("DeviceAliases=", commander.deviceAliases);
+  }
+
+  if (!commander.xplSource) {
+    var hostName = os.hostname();
+    if (hostName.indexOf('.') > 0) {
+      hostName = hostName.substring(0, hostName.indexOf('.'));
+    }
+
+    commander.xplSource = "tsx-script." + hostName;
+  }
+
+  var xpl = new Xpl(commander);
+
+  xpl.on("error", function(error) {
+    console.log("XPL error", error);
+  });
+
+  xpl.bind(function(error) {
+    if (error) {
+      console.error("Can not open xpl bridge ", error);
+      process.exit(2);
+      return;
+    }
+
+    console.log("Xpl bind succeed ");
+
+    var scriptsEngine = new ScriptsEngine(xpl, commander);
+
+    scriptsEngine.scan(params, function(error) {
+      if (error) {
+        console.error("Can not open xpl bridge ", error);
+        process.exit(3);
+        return;
       }
 
-      var xpl = new Xpl(commander);
-
-      xpl.on("error", function(error) {
-        console.log("XPL error", error);
-      });
-
-      xpl.bind(function(error) {
-        if (error) {
-          console.error("Can not open xpl bridge ", error);
-          process.exit(2);
-          return;
-        }
-
-        console.log("Xpl bind succeed ");
-
-        var scriptsEngine = new ScriptsEngine("main", commander.deviceAliases,
-            xpl);
-
-        scriptsEngine.scan(params, function(error) {
-          if (error) {
-            console.error("Can not open xpl bridge ", error);
-            process.exit(3);
-            return;
-          }
-
-          console.log("Scripts engine launched");
-        });
-      });
+      console.log("Scripts engine launched");
     });
+  });
+});
 
 commander.parse(process.argv);
 
